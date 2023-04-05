23fdb9378d5dda05f226fa44a10745c4ae17ded8
dgomez04 <dagomezmoreno@outlook.com>
2023-04-05 15:04:13 -0600

    Fully-working canvas, changes colors and grid size.

--- etchasketch.js
+++ etchasketch.js
@@ -2,26 +2,50 @@
 //get the DOM for grid
 const grid = document.querySelector("#grid");
 
-//get DOM for grid size
+//get DOM for grid size & color value
 const gridSize = document.querySelector("#grid-size");
+const colorValue = document.querySelector("#color");
+
+
+//get DOM for gridChildren nodes
+const nodes = grid.children;
+
+//DOM for eraser & rainbow button
+const eraser = document.querySelector("#eraserButton");
+
 
 //eventListener, every time value changes in gridSize
 gridSize.addEventListener('input',() =>  {
+    grid.replaceChildren();
     addGrid(gridSize.value);
 });
-
-//TODO: Make dynamic div
-const square = document.createElement('div');
 
 //function addGrid => appends squares to grid.
 function addGrid(gridSize) {
     //600px is canvas side length.
     let sideLength = (600/gridSize);
    
-    square.setAttribute("style", `width: ${sideLength}px; height: ${sideLength}px; background-color: black;`)
-
-    for(let i = 0; i < gridSize; i++) {
+    for(let i = 0; i < (gridSize**2); i++) {
+        const square = document.createElement('div');
+        square.setAttribute("style", `width: ${sideLength}px; height: ${sideLength}px;`);
+        square.style.backgroundColor= "white";
+        square.id = "square" + i;
         grid.appendChild(square);
+    }
+    changeColor();
+}
+
+//adds eventListener to every childNode under grid
+//eventListener | if mouse is moving => if left-button is clicked, change color.
+function changeColor() {
+    for(let i = 0; i < nodes.length; i++) {
+        nodes[i].onmousemove = (e) => {
+            if(e.buttons == 1) {
+                nodes[i].style.backgroundColor= `${color.value}`;
+            }
+        }       
     }
 
 }
+
+
