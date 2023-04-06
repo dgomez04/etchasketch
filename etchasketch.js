9303b905b5895b455c15d8cfc883c847516effc5
dgomez04 <dagomezmoreno@outlook.com>
2023-04-05 23:42:54 -0600

    Added rainbow and eraser functions, default grid value assigned

--- etchasketch.js
+++ etchasketch.js
@@ -1,4 +1,5 @@
 
+
 //get the DOM for grid
 const grid = document.querySelector("#grid");
 
@@ -11,13 +12,19 @@ const colorValue = document.querySelector("#color");
 const nodes = grid.children;
 
 //DOM for eraser & rainbow button
-const eraser = document.querySelector("#eraserButton");
+const eraserButton = document.querySelector("#eraserButton");
+const rainbowToggle = document.querySelector("#rainbowToggle");
 
 
 //eventListener, every time value changes in gridSize
 gridSize.addEventListener('input',() =>  {
     grid.replaceChildren();
     addGrid(gridSize.value);
+});
+
+//eventListener for eraserButton
+eraserButton.addEventListener('click', () => {
+    clearGrid();
 });
 
 //function addGrid => appends squares to grid.
@@ -41,11 +48,31 @@ function changeColor() {
     for(let i = 0; i < nodes.length; i++) {
         nodes[i].onmousemove = (e) => {
             if(e.buttons == 1) {
-                nodes[i].style.backgroundColor= `${color.value}`;
+                if(rainbowToggle.checked == true) {
+                    nodes[i].style.backgroundColor = generateColor();
+                } else {
+                    nodes[i].style.backgroundColor= `${color.value}`;
+                }
             }
         }       
     }
-
 }
 
+//function used by eraserToggle to clear grid.
+function clearGrid() {
+    for(let i = 0; i < nodes.length; i++) {
+        nodes[i].style.backgroundColor = "white";
+    }
+}
 
+//function used to generate a random RGB value
+function generateColor() {
+    let rgbValue = [];
+    for(let i = 0; i < 3; i++) {
+        rgbValue.push(Math.floor(Math.random() * 256));
+    }
+    return(`rgb(${rgbValue[0]}, ${rgbValue[1]}, ${rgbValue[2]})`);
+}
+
+//default grid value
+addGrid(gridSize.value);
